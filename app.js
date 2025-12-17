c5202f6554bc22d6aef1fcc7d50127d87e492602
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-17 18:51:12 +0530

    connect-flash

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
+const session = require('express-session');
+const flash = require('connect-flash');
 
 const cookieParser = require('cookie-parser');
 app.use(cookieParser("secretcode"));
@@ -23,6 +25,18 @@ app.use(methodOverride("_method"));
 app.engine("ejs",ejsMate);
 app.use(express.static(path.join(__dirname,"public")));
 
+const sessionOption = {
+    secret : "mysupersecretcode",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+         expires : Date.now() + 7 * 24 * 60 * 60 * 1000,
+         maxAge : 7 * 24 * 60 * 60 * 1000,
+         httpOnly : true
+    }
+}
+
+
 main().then((res)=>{
     console.log("MongoDB connetion successful");
 }).catch(err=>console.log(err));
@@ -35,6 +49,16 @@ app.get("/",(req,res)=>{
     console.log(req.cookies);
     res.send("At Home");
 })
+
+app.use(session(sessionOption));
+app.use(flash());
+
+app.use((req,res,next)=>{
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error");
+    next();
+})
+
 
 //listings routes
 app.use("/listings",listings);
