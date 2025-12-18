c65c88f6e8b28e808a1d2b4ee739c566b6baa084
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-18 14:06:33 +0530

    user model + passport added

--- models/user.js
+++ models/user.js
@@ -1,16 +1,15 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const passportLocalMongoose = require('passport-local-mongoose');
+const passportLocalMongoose = require('passport-local-mongoose').default;
 
 const userSchema = new Schema({
-    email:{
-        type: String,
-        required : true
-    },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
 });
 
-User.plugin(passportLocalMongoose);
+userSchema.plugin(passportLocalMongoose);
 
-const User = mongoose.model('user',userSchema);
-
-module.exports = User;
+module.exports = mongoose.model("User", userSchema);
