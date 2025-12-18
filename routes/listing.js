3a5e44dff16a8e5adc7c20b37d3840e7772eb2b7
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-18 16:32:52 +0530

    new listing's owner adding

--- routes/listing.js
+++ routes/listing.js
@@ -31,6 +31,7 @@ router.get("/new",isLoggedIn,(req,res)=>{
 
 router.post("/",isLoggedIn, validateListing, wrapAsync(async(req,res)=>{
    const newListing = new Listing(req.body.listing); 
+   newListing.owner = req.user._id;
    await newListing.save();
    req.flash("success","New Listing Created");
    res.redirect("/listings");
