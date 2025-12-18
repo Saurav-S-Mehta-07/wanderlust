3527ac4a2520c2e68d83b108518936ceabc152f9
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-18 14:55:56 +0530

    user route created

---
+++ routes/user.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
+
+router.get("/signup",(req,res)=>{
+    res.render("users/signup");
+});
+
+router.post("/signup",wrapAsync(async(req,res)=>{
+    try{
+        let {username, email, password} = req.body;
+        const newUser = new User({email,username});
+        const registeredUser = await User.register(newUser,password);
+        console.log(registeredUser);
+        req.flash("success","Welcome to ChillCasa");
+        res.redirect("/listings");
+    } 
+    catch(err){
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+}));
+
+router.get("/login",(req,res)=>{
+    res.render("users/login.ejs");
+})
+
+router.post("/login",
+    passport.authenticate("local",
+        {failureRedirect :'/login',
+        failureFlash:true
+    }),
+    async(req,res)=>{
+        req.flash("success","Welcome back to ChillCasa");
+        res.redirect("/listings");
+});
+
+module.exports = router;
