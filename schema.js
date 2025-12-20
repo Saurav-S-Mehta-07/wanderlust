0146740ccfbb89b03b4bd98472dc70198954a141
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-20 17:20:11 +0530

    joi validation added on category

--- schema.js
+++ schema.js
@@ -1,19 +1,35 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 module.exports.ListingSchema = Joi.object({
-      listing : Joi.object({
-        title : Joi.string().required(),
-        description: Joi.string().required(),
-        price: Joi.number().required().min(0),
-        location: Joi.string().required(),
-        country: Joi.string().required(),
-        image : Joi.string().allow("",null),
-      }).required()
+  listing: Joi.object({
+    title: Joi.string().required(),
+    description: Joi.string().required(),
+    price: Joi.number().min(0).required(),
+    location: Joi.string().required(),
+    country: Joi.string().required(),
+    image: Joi.object({
+      filename: Joi.string().allow("", null),
+      url: Joi.string().uri().allow("", null),
+    }).required(),
+    category: Joi.string()
+      .valid(
+        "trending",
+        "forests",
+        "rooms",
+        "iconic cities",
+        "mountains",
+        "castles",
+        "amazing pools",
+        "farms",
+        "camping"
+      )
+      .required(),
+  }).required(),
 });
 
 module.exports.reviewSchema = Joi.object({
-   review : Joi.object({
-     rating : Joi.number().required().min(1).max(5),
-     comment : Joi.string().required(),
-   }).required()
-})
+  review: Joi.object({
+    rating: Joi.number().min(1).max(5).required(),
+    comment: Joi.string().required(),
+  }).required(),
+});
