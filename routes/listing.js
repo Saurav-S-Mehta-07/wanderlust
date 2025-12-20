60cfe6d51fe7db5eba36b53b51459a939bda914d
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-20 11:23:18 +0530

    cloud- for image added

--- routes/listing.js
+++ routes/listing.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const wrapAsync = require('../utils/wrapAsync.js');
-const Listing = require("../models/listing.js");
 const { isLoggedIn, isOwner, validateListing } = require('../middleware.js');
 const listingController = require("../controllers/listings.js");
-
 const multer  = require('multer');
-const upload = multer({ dest: 'uploads/' })
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage })
 
 router.route("/")
 .get(wrapAsync(listingController.Index))
-.post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
+// .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing));
 
 //local storage
-// .post(upload.single('listing[image]'),(req,res)=>{
-//     res.send(req.file);
-// })
+.post(upload.single("listing[image]"),(req,res)=>{
+    res.send(req.file);
+})
 
 router.get("/new",isLoggedIn,listingController.renderNewForm);
 
