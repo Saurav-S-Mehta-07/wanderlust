5814b0bc99f63c94820e2195bf113f9b1e66a934
Saurav s Mehta <sauravmehta631@gmail.com>
2025-12-21 17:51:36 +0530

    category  name updated

--- schema.js
+++ schema.js
@@ -10,7 +10,7 @@ module.exports.ListingSchema = Joi.object({
         image : Joi.string().allow("",null),
         category: Joi.string()
         .valid(
-            "trending", "rooms", "iconic cities", "mountains", "castles", "amazing pool", "camping", "farms", "forest", "arctic"
+            "trending", "rooms", "iconic cities", "mountains", "castles", "amazing pools", "camping", "farms", "forests", "arctic"
         )
       }).required()
 });
