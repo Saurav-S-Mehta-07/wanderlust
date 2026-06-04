2d3b86945898ca22b676fe29dfd4267f0ff68b0c
Saurav s Mehta <sauravmehta631@gmail.com>
2026-06-04 19:17:32 +0530

    updated

--- init/index.js
+++ init/index.js
@@ -2,25 +2,19 @@ const mongoose = require("mongoose");
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/chillcasa";
-
-main()
-  .then(() => {
-    console.log("connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+main().then(() => {
+    console.log("connection successful");
+}).catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect(MONGO_URL);
+    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
 }
 
 const initDB = async () => {
-  await Listing.deleteMany({});
-  initData.data = initData.data.map((obj)=>({...obj, owner : '6943da0e7d25d93b764a7fdb'}));
-  await Listing.insertMany(initData.data);
-  console.log("data was initialized");
-};
+    await Listing.deleteMany({});
+    initData.data = initData.data.map((obj)=>({...obj, owner:'686ccfdec6b7f2b24c3fcd5f'}));
+    await Listing.insertMany(initData.data); 
+    console.log("data was initialized");
+}
 
 initDB();
